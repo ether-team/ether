6d91b8b3a6074c5219d362fd979b15585d46a2c7
Ed Bartosh <bartosh@gmail.com>
2011-03-04 18:05:25 +0200

    fixed python packaging

--- setup.py
+++ setup.py
@@ -9,9 +9,14 @@ setup(
     url='https://github.com/ether-team/ether',
     author='BIFH & OBS teams',
     author_email='bifh-team@nokia.com',
-    packages=find_packages(exclude=['ez_setup']),
-    data_files=[('share/vcs-hooks/svn', ['hooks/svn/ether-post-commit']),
-                ('share/vcs-hooks/git', ['hooks/git/ether-post-receive'])],
+    packages=find_packages(exclude=['configs']),
+    data_files=[
+        ('share/vcs-hooks/svn', ['hooks/svn/ether-post-commit']),
+        ('share/vcs-hooks/git', ['hooks/git/ether-post-receive']),
+        ('/etc/ether', ['configs/common.py', 'configs/svn_postcommit.py',
+                        'configs/test_consumer.py']),
+    ],
+    scripts = ['examples/test_consumer'],
     classifiers=[
         'Operating System :: OS Independent',
         'Programming Language :: Python',
